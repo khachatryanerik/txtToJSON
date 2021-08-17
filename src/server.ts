35eadfaf04bfd98b4cd4b11b36808757fa6dcb41
Erik <erikxachatryan528517@gmail.com>
2021-08-17 05:48:58 +0400

    Environment variables added

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import Express from "express";
 import routers from "./routers/routers";
 import users from "./routers/users";
+import dotenv from "dotenv";
 
 const app = Express();
 
-app.listen(3311);
+dotenv.config();
+
+app.listen(process.env.PORT);
 app.use("/", routers);
 app.use("/users/", users);
